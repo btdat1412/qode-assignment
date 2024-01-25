22ee64836ea4618fa05522d89d0c229cfaa90040
20120265 <lixhen2002@gmail.com>
2024-01-25 23:10:45 +0700

    fix env name

--- src/service/s3.ts
+++ src/service/s3.ts
@@ -4,9 +4,9 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import crypto from "crypto";
 
 const s3Client = new S3Client({
-  region: process.env.AWS_BUCKET_REGION!,
+  region: process.env.AWS_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
